755f384582ab08edcea8e25ddafd4a9a7f3273cd
ratipatkub <684259029@webmail.npru.ac.th>
2026-02-27 11:45:18 +0700

    connect and select

---
+++ helloworld.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ratipat</title>
+</head>
+
+<body>
+    Hello World of America, UMAMUSUME HERE!!
+
+    <?php
+    echo "Hello World of PHP with echo statement";
+    ?>
+
+
+</body>
+
+</html>
